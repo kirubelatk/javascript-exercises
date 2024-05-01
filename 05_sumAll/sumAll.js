7cc26aeeb8950d6a4bc4c9c0949f232094e48478
kirubelatk <kirubelateka@gmail.com>
2024-05-01 22:24:50 +0200

    modified the js to pass all the test on loops

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(x,y) {
+    let sum = 0;
+    if(isNaN(x) || isNaN(y) || typeof x !== "number" || typeof y !== "number"){
+        return "ERROR"
+    }
+    else if(x < 0 || y < 0){
+        return "ERROR"
+       }
+    else if(x < y){
+        for(let i = x;i<=y;i++){
+            sum += i;
+     };  
+    }
+    else if(y < x){
+        for(let i = y;i<=x;i++){
+            sum += i;
+     }; 
+    }
+  
+return sum;
 };
 
 // Do not edit below this line
