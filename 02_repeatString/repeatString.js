0488b3a64ab4291bc4cd7c40c3d77da3aae540df
kirubelatk <kirubelateka@gmail.com>
2024-05-01 21:09:14 +0200

    modified the js to pass the test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,20 @@
-const repeatString = function() {
-
+const repeatString = function(x,y) {
+   let result=''
+   if(y>0){
+    for(let i=0;i<y;i++){
+        result+=x
+        }
+        return result
+   }
+   else if(y === 0){
+    return ""
+   }
+   else{
+    return "ERROR"
+   }
+   
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
