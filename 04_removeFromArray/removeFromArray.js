5f50abc57e3c54607af1ba58d7b408c007ffa2be
kirubelatk <kirubelateka@gmail.com>
2024-05-01 22:04:13 +0200

    modified the js to pass the teston removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
 
-};
+const removeFromArray = function (array, ...args) {
+    let newArray = [];
+
+     array.forEach((ele) => {
+      if(!args.includes(ele)){  
+        newArray.push(ele)
+    }});
+return newArray
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
