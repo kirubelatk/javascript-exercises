15227173eeb292f05c6367b26954281ed2c38a04
kirubelatk <kirubelateka@gmail.com>
2024-05-01 22:39:07 +0200

    done the exercises on the leapYears.js

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,19 @@
-const leapYears = function() {
-
-};
+const leapYears = function(x) {
+     if(x % 4 === 0 ){
+        if(x % 100 === 0 && x % 400 === 0 ){
+            return true;
+        }
+        else if(x % 100 ===0 && x % 400 !== 0){
+            return false;
+         }
+        else{
+        return true;
+        }
+     }
+else {
+    return false
+}
+}
 
 // Do not edit below this line
 module.exports = leapYears;
